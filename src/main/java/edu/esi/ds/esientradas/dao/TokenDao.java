8dd6769b6f0ebac1c8246d529799e94792a9002e
IvanMB07 <ivan.moreno7@alu.uclm.es>
2026-02-26 19:34:11 +0100

    Delete src directory

--- src/main/java/edu/esi/ds/esientradas/dao/TokenDao.java
+++
@@ -1,8 +0,0 @@
-package edu.esi.ds.esientradas.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.esi.ds.esientradas.model.Token;
-
-public interface TokenDao extends JpaRepository<Token, String> {
-}
